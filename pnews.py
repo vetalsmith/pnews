fdef4c82803570b8a6743497d664aade0fc44495
superadm <komatoz1804@gmail.com>
2018-10-07 13:08:05 +1100

    save

--- pnews.py
+++ pnews.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 
 html_doc = requests.get('https://news.yandex.ru/index.html')
 soup = BeautifulSoup(html_doc.text, 'html.parser')
-print(soup.prettify())
+soup = soup.find_all(class_='story__topic')
+for row in soup:
+	print(row.prettify())
